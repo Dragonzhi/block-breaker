492b70a83f9e733d4a260c4c998a183c52b0d6f9
zhilong-dragonz <zhi3248569738@126.com>
2025-06-27 16:51:16 +0800

    ·修改声音播放等设置

--- 打砖块/resources_manager.cpp
+++ 打砖块/resources_manager.cpp
@@ -84,51 +84,10 @@ void ResourcesManager::load()
 	}
 
 	flip_image("paddle_blue_right", "paddle_blue_left", 1);
-	//flip_image("player_dead_right", "player_dead_left", 6);
-	//flip_image("player_fall_right", "player_fall_left", 5);
-	//flip_image("player_idle_right", "player_idle_left", 5);
-	//flip_image("player_jump_right", "player_jump_left", 5);
-	//flip_image("player_run_right", "player_run_left", 10);
-	//flip_image("player_roll_right", "player_roll_left", 7);
 
 	//flip_atlas("sword_left", "sword_right");
-	//flip_atlas("enemy_aim_left", "enemy_aim_right");
-	//flip_atlas("enemy_dash_in_air_left", "enemy_dash_in_air_right");
-	//flip_atlas("enemy_dash_on_floor_left", "enemy_dash_on_floor_right");
-	//flip_atlas("enemy_fall_left", "enemy_fall_right");
-	//flip_atlas("enemy_idle_left", "enemy_idle_right");
-	//flip_atlas("enemy_jump_left", "enemy_jump_right");
-	//flip_atlas("enemy_run_left", "enemy_run_right");
-	//flip_atlas("enemy_squat_left", "enemy_squat_right");
-	//flip_atlas("enemy_throw_barb_left", "enemy_throw_barb_right");
-	//flip_atlas("enemy_throw_silk_left", "enemy_throw_silk_right");
-	//flip_atlas("enemy_throw_sword_left", "enemy_throw_sword_right");
-
-	//flip_atlas("enemy_vfx_dash_in_air_left", "enemy_vfx_dash_in_air_right");
-	//flip_atlas("enemy_vfx_dash_on_floor_left", "enemy_vfx_dash_on_floor_right");
 
 	//load_audio(_T(R"(resources\audio\bgm.mp3)"),			_T("bgm"));
-	//load_audio(_T(R"(resources\audio\barb_break.mp3)"),		_T("barb_break"));
-	//load_audio(_T(R"(resources\audio\bullet_time.mp3)"),	_T("bullet_time"));
-
-	//load_audio(_T(R"(resources\audio\enemy_dash.mp3)"),		_T("enemy_dash"));
-	//load_audio(_T(R"(resources\audio\enemy_run.mp3)"),		_T("enemy_run"));
-	//load_audio(_T(R"(resources\audio\enemy_hurt_1.mp3)"),	_T("enemy_hurt_1"));
-	//load_audio(_T(R"(resources\audio\enemy_hurt_2.mp3)"),	_T("enemy_hurt_2"));
-	//load_audio(_T(R"(resources\audio\enemy_hurt_3.mp3)"),	_T("enemy_hurt_3"));
-	//load_audio(_T(R"(resources\audio\enemy_throw_barbs.mp3)"), _T("enemy_throw_barbs"));
-	//load_audio(_T(R"(resources\audio\enemy_throw_silk.mp3)"), _T("enemy_throw_silk"));
-	//load_audio(_T(R"(resources\audio\enemy_throw_sword.mp3)"), _T("enemy_throw_sword"));
-
-	//load_audio(_T(R"(resources\audio\player_attack_1.mp3)"), _T("player_attack_1"));
-	//load_audio(_T(R"(resources\audio\player_attack_2.mp3)"), _T("player_attack_2"));
-	//load_audio(_T(R"(resources\audio\player_attack_3.mp3)"), _T("player_attack_3"));
-	//load_audio(_T(R"(resources\audio\player_dead.mp3)"), _T("player_dead"));
-	//load_audio(_T(R"(resources\audio\player_hurt.mp3)"), _T("player_hurt"));
-	//load_audio(_T(R"(resources\audio\player_jump.mp3)"), _T("player_jump"));
-	//load_audio(_T(R"(resources\audio\player_land.mp3)"), _T("player_land"));
-	//load_audio(_T(R"(resources\audio\player_roll.mp3)"), _T("player_roll"));
-	//load_audio(_T(R"(resources\audio\player_run.mp3)"),	 _T("player_run"));
 	
 	std::cout << "资源加载完成" << std::endl;
 }
